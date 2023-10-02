9f813a77b2266b6a80fff50c58637c4d9811818e
Dempster420 <calum.dempster@students.mq.edu.au>
2023-10-02 11:46:34 +1100

    Imported usestate

--- reedmi/src/App.js
+++ reedmi/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import axios from "axios";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
   return (
