8ee7205d1464e29e7948b620ed268d7cf23c885e
Dempster420 <calum.dempster@students.mq.edu.au>
2023-10-02 12:10:06 +1100

    Added sample data to backend server

--- reedmi/server/index.js
+++ reedmi/server/index.js
@@ -4,8 +4,19 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const fs = require("fs");
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+let posts = JSON.parse(fs.readFileSync("data.json")).posts;
+
+app.get("/api/posts", (_, response) => {
+  console.log("received request post")
+  //responds requested product data
+  //get request retrieving different parts of json file
+  response.send(posts);
 });
 
 const port = 3001;
--- reedmi/src/App.js
+++ reedmi/src/App.js
@@ -4,6 +4,20 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 function App() {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    console.log("effect is running");
+    axios
+    .get("https://clothesandco.onrender.com/api/products")
+    .then((response) => {
+      //get request to retrieve product information from the sampledata.json from the back end server
+      console.log("we have a response", response);
+      setPosts(response.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
