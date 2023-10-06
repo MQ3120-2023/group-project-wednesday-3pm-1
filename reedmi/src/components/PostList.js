35b3f87282e0419a0c34d1b240369c7658457ff7
Dempster420 <calum.dempster@students.mq.edu.au>
2023-10-06 13:24:24 +1100

    restyled displayed posts

--- reedmi/src/components/PostList.js
+++ reedmi/src/components/PostList.js
@@ -1,12 +1,13 @@
 import React from "react";
 //import { Link } from "react-router-dom";
+import '../App.css';
 
 function PostList({posts}){
     return(
         <div>
             {posts.map(post => (
             <div key={post.id} className="post-container">
-              <h1>{post.postTitle}</h1>
+              <h1 id="postTitle" >{post.postTitle}</h1>
               <figure>
                 <img className="postImage" src={post.img} alt={post.postTitle} />
               </figure>
