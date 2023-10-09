d0feef4121154165e7bce190398fdfefe6f87416
Mifta Alam <tilovaalam@gmail.com>
2023-10-10 00:08:20 +1100

    This is where the main express application instance is imported from app.js, and the server is started on port 3001

---
+++ reedmi/server/proxyserver.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+const app = require('./app') // importing the main Express application instance from the app.js file
+// `app` now holds a reference to the Express application instance that was exported from  `app.js`
+
+const PORT = 3001
+// using `app` to start the express server 
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
