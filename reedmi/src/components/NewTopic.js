6368a0b9d4d8d7b2ce0a2456d292f411737118d1
Dempster420 <calum.dempster@students.mq.edu.au>
2023-10-20 15:38:04 +1100

    Implemented add new topic feature, need to debug

--- reedmi/src/components/NewTopic.js
+++ reedmi/src/components/NewTopic.js
@@ -3,12 +3,27 @@ import { Link } from "react-router-dom";
 import '../App.css';
 
 function NewTopic(){
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("event", event)
+        console.log("value", event.target[0].value)
+        //MIFF, please add this to backend 
+        // Sends  posted comment to backend server
+        // axios
+        //   .post(`http://localhost:3001/api/topics/`, {
+        //     newTopic: event.target[0].value
+        //   })
+        //   .then(() => {
+        //     window.reload()
+        //   });
+      };
     
     return(
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Name of Topic</label>
-                <input type="text"id="topicName"
+                <input type="text"id="topicName" maxLength={25}
                 />
                 <button type="submit">Submit</button>
             </form>
