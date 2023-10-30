171341832cb6116b303e4b1b7e2b75fb06cdb456
Ryan Kontos <ryanjkontos@gmail.com>
2023-10-31 03:14:46 +1100

    Fixed user schema

--- reedmi/server/models/user.js
+++ reedmi/server/models/user.js
@@ -2,8 +2,24 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true, 
+        strict: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        strict: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 module.exports = mongoose.model('User', userSchema);
