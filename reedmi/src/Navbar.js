e0860886f1d631788d344acc019cecf058bcee07
AhmedDK98 <ahmeddbouk@hotmail.com>
2023-11-04 20:27:54 +1100

    updated deployment.md

--- reedmi/src/Navbar.js
+++ reedmi/src/Navbar.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-
+/*Function responsible for navigating through the website in the header*/
 function Navbar() {
     return (
         <header className="App-header">
             <div className="header-contents">
                 <p className="header-title header-link">
-                    <Link className="header-link" to={"/home"}>
+                    <Link className="header-link" to={"/home"}> {/* Link back to Home page */}
                         ReedMi
                     </Link>
                 </p>
-                <div className="navOptions">
-                    <Link className="header-link header-link-secondary" to="/techNews">
+                <div className="navOptions"> 
+                    <Link className="header-link header-link-secondary" to="/techNews"> {/* Link to TechNews section */}
                         TechNews
                     </Link>
                     <div className="navbar-links">
-                        <Link id="add-new-post" to="/createNewPost">
+                        <Link id="add-new-post" to="/createNewPost"> {/* Link to Add new posts */}
                             Add New Post +
                         </Link>
                     </div>
-
+                    
                     <div className="navbar-links">
-                        <Link id="add-new-post" to="/login">
+                        <Link id="add-new-post" to="/login"> {/* Link for the user to sign out */}
                             Sign Out
                         </Link>
                     </div>
