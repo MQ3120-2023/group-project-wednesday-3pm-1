f5c34f7c447978c9d96d6e5d4060f6894af6046e
Ryan Kontos <ryanjkontos@gmail.com>
2023-11-05 20:45:15 +1100

    Added route and component detail to readme

--- reedmi/src/components/Navbar.js
+++ reedmi/src/components/Navbar.js
@@ -6,35 +6,48 @@ import { baseURL } from '../apiClient';
 
 function Navbar() {
     const [username, setUsername] = useState('');
+    const [isLoading, setIsLoading] = useState(true);  // New state to track loading status
 
     useEffect(() => {
         apiClient.get('/api/auth/current_user')
-            .then(response => setUsername(response.data.username))
-            .catch(error => console.error(error));
+            .then(response => {
+                setUsername(response.data.username);
+                setIsLoading(false);  // Set loading status to false once data is fetched
+            })
+            .catch(error => {
+                console.error(error);
+                setIsLoading(false);  // Set loading status to false even if there is an error
+            });
     }, []);
 
     let url = `${baseURL}/api/auth/logout`;
+
+    // Conditional rendering based on loading status
+    if (isLoading) {
+        return  <header className="App-header">
+        <div className="header-contents">
+            </div>
+        </header>
+    }
 
     return (
         <header className="App-header">
             <div className="header-contents">
                 <p className="header-title header-link">
-                    <Link className="header-link" to={"/home"}> {/* Link back to Home page */}
+                    <Link className="header-link" to={"/home"}>
                         ReedMi
                     </Link>
                 </p>
                 <div className="navOptions"> 
-                    <Link className="header-link header-link-secondary" to="/techNews"> {/* Link to TechNews section */}
+                    <Link className="header-link header-link-secondary" to="/techNews">
                         TechNews
                     </Link>
                     <div className="navbar-links">
-                        <Link id="add-new-post" to="/createNewPost"> {/* Link to Add new posts */}
+                        <Link id="add-new-post" to="/createNewPost">
                             Add New Post +
                         </Link>
                     </div>
-
                     <div className="navbar-links">
-
                         <Link id="add-new-post" to={`${url}`}>
                             Log Out '{username}'
                         </Link>
@@ -44,4 +57,5 @@ function Navbar() {
         </header>
     );
 }
+
 export default Navbar;
